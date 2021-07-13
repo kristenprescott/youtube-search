2867d71447735194da78148036efe13960bf593d
KayPea <prescottdarling@gmail.com>
2021-07-13 10:21:29 -0400

    simple and works, search Youtube for videos and display them/play them

--- src/App.js
+++ src/App.js
@@ -1,36 +1,48 @@
-import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import { Grid } from "@material-ui/core";
+
+import { SearchBar, VideoList, VideoDetail } from "./components";
+
 import youtube from "./api/youtube";
 
-// const App = () => {
-//   return (
-//     <div>
-//       <h1>Youtube Clone</h1>
-//     </div>
-//   );
-// };
+const App = () => {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState({ id: {}, snippet: {} });
 
-class App extends React.Component {
-  render() {
-    return (
-      <Grid justify="center" container spacing={16}>
-        <Grid item xs={11}>
-          <Grid container spacing={16}>
-            <Grid item xs={12}>
-              {/* SEARCHBAR */}
-            </Grid>
-            <Grid item xs={8}>
-              {/* VIDEO DETAILS */}
-            </Grid>
-            <Grid item xs={4}>
-              {/* VIDEO LIST */}
-            </Grid>
+  return (
+    <Grid style={{ justifyContent: "center" }} container spacing={10}>
+      <Grid item xs={11}>
+        <Grid container spacing={10}>
+          <Grid item xs={12}>
+            <SearchBar onSubmit={handleSubmit} />
+          </Grid>
+          <Grid item xs={8}>
+            <VideoDetail video={selectedVideo} />
+          </Grid>
+          <Grid item xs={4}>
+            <VideoList videos={videos} onVideoSelect={setSelectedVideo} />
           </Grid>
         </Grid>
       </Grid>
-    );
+    </Grid>
+  );
+
+  async function handleSubmit(searchTerm) {
+    const {
+      data: { items: videos },
+    } = await youtube.get("search", {
+      params: {
+        part: "snippet",
+        maxResults: 5,
+        // TODO - add a new API key.
+        key: process.env.REACT_APP_API_KEY,
+        q: searchTerm,
+      },
+    });
+
+    setVideos(videos);
+    setSelectedVideo(videos[0]);
   }
-}
+};
 
 export default App;
--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,14 +1,5 @@
 import axios from "axios";
 
-const apiKey = process.env.REACT_APP_API_KEY;
-
 export default axios.create({
-  baseUrl: "https://www.googleapis.com/youtube/v3",
-  params: {
-    // like typing https://www.googleapis.com/youtube/v3?part=snippet
-    // returns videos
-    part: "snippet",
-    maxResults: 5,
-    key: apiKey,
-  },
+  baseURL: "https://www.googleapis.com/youtube/v3",
 });
