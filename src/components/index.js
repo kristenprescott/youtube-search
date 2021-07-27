871c4c3ef8ce21219be6341ffded49cbc766ee4c
KayPea <prescottdarling@gmail.com>
2021-07-27 09:20:43 -0400

    Updates

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as NavBar } from "./NavBar";
-export { default as SearchBar } from "./SearchBar";
-export { default as VideoDetail } from "./VideoDetail";
-export { default as VideoList } from "./VideoList";
-export { default as VideoItem } from "./VideoItem";
